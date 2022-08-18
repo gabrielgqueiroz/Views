2f1910558c431f5d8df3050dcbcaf4d1a0a17d20
gabrielgqueiroz <ggabriel12.gg@gmail.com>
2022-08-17 22:02:21 -0300

    Adição de icone e tratamento de eventos

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.views.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
+    private Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.limparBt.setOnClickListener(view -> {
+            activityMainBinding.nomeEt.setText("");
+            activityMainBinding.sobrenomeEt.setText("");
+            activityMainBinding.emailEt.setText("");
+            activityMainBinding.estadoCivilSp.setSelection(0);
+            activityMainBinding.femininoRb.setChecked(true);
+            pessoa = null;
+        });
+
+        activityMainBinding.salvarBt.setOnClickListener(view -> {
+            //Toast.makeText(this, activityMainBinding.estadoCivilSp.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            pessoa = new Pessoa(
+                    activityMainBinding.nomeEt.getText().toString(),
+                    activityMainBinding.sobrenomeEt.getText().toString(),
+                    activityMainBinding.emailEt.getText().toString(),
+                    activityMainBinding.estadoCivilSp.getSelectedItem().toString(),
+                    //activityMainBinding.femininoRb.isChecked() ? "feminino" : "masculino"
+                    activityMainBinding.femininoRb.isChecked() ? getString(R.string.feminino) : getString(R.string.masculino)
+                    //activityMainBinding.femininoRb.isChecked() ? activityMainBinding.femininoRb.getText().toString() : activityMainBinding.masculinoRb.getText().toString()
+            );
+            Toast.makeText(this, pessoa.toString(), Toast.LENGTH_SHORT).show();
+        });
     }
 }
